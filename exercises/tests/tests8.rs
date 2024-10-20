4d698d33fb6d0cff765ebb18c9f3bbba39d12f14
mazhaoyang@nudt.edu.cn <mazhaoyang@nudt.edu.cn>
2024-10-20 23:51:09 +0800

    update

--- exercises/tests/tests8.rs
+++ exercises/tests/tests8.rs
@@ -7,20 +7,18 @@
 // Execute `rustlings hint tests8` or use the `hint` watch subcommand for a
 // hint.
 
-use std::time::{SystemTime, UNIX_EPOCH};
 
-fn main() {
-    // In tests7, we should set up an environment variable
-    // called `TEST_FOO`. Print in the standard output to let
-    // Cargo do it.
-    let timestamp = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
+fn main() {}
 
-    // In tests8, we should enable "pass" feature to make the
-    // testcase return early. Fill in the command to tell
-    // Cargo about that.
-    println!("cargo:rustc-cfg=pass");
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_success() {
+        #[cfg(pass)]
+        return;
+
+        panic!("no cfg set");
+    }
 }
